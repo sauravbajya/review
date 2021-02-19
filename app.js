e08df7d1f6fcf865835440bef591030f734a43ab
Saurav Bajracharya <sauravbajya@gmail.com>
2021-02-19 20:55:48 +0545

    final commit

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
 let person =[
     {
-        pic : 'user-img.jpg',
+        pic : '/img/user-img1.jpg',
         name : 'Tori Doi',
         job : 'Engineer',
         review : 'Enim optio voluptas molestias pariatur error, vel saepe cum vero minima culpa dignissimos eius, incidunt eveniet aspernatur perferendis ipsum quod mollitia commodi.'
     },
     {
-        pic : 'user-img.jpg',
+        pic : '/img/user-img.jpg',
         name : 'Saurav Bajracharya',
         job : 'Web Developer',
-        review : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim optio voluptas molestias pariatur error, vel saepe cum vero minima culpa dignissimos eius, incidunt eveniet aspernatur perferendis ipsum quod mollitia commodi.'
+        review : 'Lorem ipsum dolor luptas molestias pariatur error, vel saepe cum vero minima culpa dignissimos eius, incidunt eveniet aspernatur perferendis ipsum quod mollitia commodi.'
     },
     {
-        pic : 'user-img.jpg',
+        pic : '/img/user-img2.jpg',
         name : 'Rob Lucci',
         job : 'Doctor',
+        review : 'adipisicing elit. Enim '
+    },
+    {
+        pic : '/img/user-img3.jpg',
+        name : 'Furax Devkota',
+        job : 'Sleeper',
+        review : 'arror, vel saepe cum vero minima culpa dignissimos eius, incidunt eveniet aspernatur perferendis ipsum quod mollitia commodi.'
+    },
+    {
+        pic : '/img/user-img4.jpg',
+        name : 'Fisher Man',
+        job : 'Pilot',
         review : 'adipisicing elit. Enim optio voluptas molestias pariatur error, vel saepe cum vero minima culpa dignissimos eius, incidunt eveniet aspernatur perferendis ipsum quod mollitia commodi.'
+    },
+    {
+        pic : '/img/user-img5.jpg',
+        name : 'Sulavi Bara',
+        job : 'Umemployed',
+        review : ' molestias pariatur error, vel saepe cum vero minima culpa dignissimos eius, incidunt eveniet aspernatur perferendis ipsum quod mollitia commodi.'
+    },
+    {
+        pic : '/img/user-img6.jpg',
+        name : 'Choco Polom',
+        job : 'Surfer',
+        review : 'adipisicing elit. Enim optio voluptas molestias pariatur error, vel saepe cum vero minima culpa dignissimos eius, incidunt eveniet aspernatur perferendis ipsum quod mollitia commodi.'
+    },
+    {
+        pic : '/img/user-img7.jpg',
+        name : 'Bob Builder',
+        job : 'Miner',
+        review : 'adipisicing elit. Enim optio culpa dignissimos eius, incidunt eveniet aspernatur perferendis ipsum quod mollitia commodi.'
+    },
+    {
+        pic : '/img/user-img8.jpg',
+        name : 'Leroy Mole',
+        job : 'Carpenter',
+        review : 'adipisicing elit. Enim optio voluptas molestias pariatur error, vel saepe cum vero minima culpa dmollitia commodi.'
     }
+    
 ]
 let prev = document.querySelector('#prev')
 let next = document.querySelector('#next')
@@ -48,9 +85,23 @@ rand.addEventListener('click', () => {
 
 prev.addEventListener('click',() => 
 {
-    id--
+    id--;
+    if(id < 0){
+        id = person.length-1;
+    }
     change();
 })
+
+next.addEventListener('click',() => 
+{
+    id++;
+    if(id > person.length-1){
+        id = 0;
+    }
+    console.log(id);
+    change();
+})
+
 
 
 
